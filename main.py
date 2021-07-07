d8321f73352539508654ed72d382d0a42a93db51
Taner Ahmed <tanerahmed87@gmail.com>
2021-07-07 14:13:59 +0300

    Fix trirth commit

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     # second commit
     print('SECOND COMMIT')
+    print('Thirty COMMIT   111')
 
 
 # Press the green button in the gutter to run the script.
