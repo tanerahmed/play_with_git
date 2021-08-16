4a0fe84faf85a5dc6488515fa22fe3c6ec59ce52
tanerahmed <tanerahmed87@gmail.com>
2021-08-16 04:47:37 -0700

    ttt commit

--- main.py
+++ main.py
@@ -15,6 +15,6 @@ if __name__ == '__main__':
     print(run("status"))
     run("add", "--all",)
     run("commit", "-m", 'ttt commit')
-    run("push", "-u", "origin", "master")
+    run("push",)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
