e83610bce38f1066a11e16fd67df3915cd9b910e
tanerahmed <tanerahmed87@gmail.com>
2021-08-16 06:05:53 -0700

    ttt commit

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ def run(*args):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # print(run("status"))
-    # run("add", "--all",)
-    # run("commit", "-m", 'ttt commit')
+    tt()
+    print(run("status"))
+    run("add", "--all",)
+    run("commit", "-m", 'ttt commit')
     run("push", "-u", "origin", "master")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
