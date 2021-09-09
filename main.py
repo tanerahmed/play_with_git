154000c1599979cfba17bd22d10087743988f50c
Taner Ahmed <tanerahmed87@gmail.com>
2021-09-09 17:41:45 +0300

    m

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('after reset')
     print('09.09')
+    print('09.09 -------- 2222222')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
